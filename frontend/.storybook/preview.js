cf0648b0e6edd9f832c03077a81ed4942fb6ca90
Frontastic Developers GitHub workflow <frontastic-developers-github@frontastic.cloud>
2023-11-30 10:16:56 +0000

    Update from main repository

---
+++ frontend/.storybook/preview.js
@@ -0,0 +1,38 @@
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+import { sdk } from '../sdk';
+import 'tailwindcss/tailwind.css';
+import '../styles/app.css';
+import 'react-loading-skeleton/dist/skeleton.css';
+import theme from './theme';
+
+export const parameters = {
+  docs: {
+    theme: theme,
+  },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
+    locales: ['en_US', 'de_DE'],
+    locale: 'en_US',
+  },
+  options: {
+    storySort: {
+      order: ['Style guide', '*'],
+    },
+    showPanel: false,
+  },
+};
+
+const StoryWrapper = ({ Story }) => {
+  sdk.configureForNext('en');
+
+  return <Story />;
+};
+
+export const decorators = [(Story) => <StoryWrapper Story={Story} />];
