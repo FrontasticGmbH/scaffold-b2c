ccc9b1b2669eec8007b3583e0d49c8d0ea5d0d13
Frontastic Developers GitHub workflow <frontastic-developers-github@frontastic.cloud>
2025-08-08 11:06:00 +0000

    Update from main repository

--- frontend/components/commercetools-ui/organisms/not-found/index.tsx
+++ frontend/components/commercetools-ui/organisms/not-found/index.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import { useTranslations } from 'use-intl';
 import Button from 'components/commercetools-ui/atoms/button';
-import Image, { ImageProps } from 'components/commercetools-ui/atoms/image';
+import Image from 'components/commercetools-ui/atoms/image';
 import Link from 'components/commercetools-ui/atoms/link';
 
-type NotFoundProps = {
-  image?: ImageProps;
-};
-
-const NotFound = ({ image }: NotFoundProps) => {
+const NotFound = () => {
   const translate = useTranslations();
 
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-0 py-80 md:my-60 md:py-120 lg:flex-row-reverse lg:gap-64">
-      <div className="relative mx-10 w-152 md:w-200 lg:w-350">
-        <Image {...image} style={{ width: '100%', height: 'auto' }} />
+    <div className="my-175 flex w-full flex-col items-center justify-center gap-0 lg:flex-row-reverse lg:gap-64">
+      <div className="relative mx-10 w-126 md:w-200 lg:w-350">
+        <Image
+          src="/images/shy-lens.png"
+          alt={translate('common.page-notFound')}
+          style={{ width: '100%', height: 'auto' }}
+        />
       </div>
       <div className="mx-60 mt-24 text-center md:mx-0 lg:mt-0 lg:text-left">
-        <h1 className="text-18 leading-[1.25] text-gray-700 md:text-22 lg:text-32">
-          {translate('common.page-notFound')}
-        </h1>
-        <p className="mb-24 mt-12 text-sm leading-[1.5] text-gray-600 md:text-16 lg:mb-28 lg:mt-24">
-          {translate('common.page-notFound-desc')}
-        </p>
+        <h1 className="text-32 leading-[1.25]">{translate('common.page-notFound')}</h1>
+        <p className="mb-24 mt-12 leading-[1.5] lg:mb-28 lg:mt-24">{translate('common.page-notFound-desc')}</p>
         <Link link="/">
           <Button variant="primary">{translate('common.page-notFound-cta')}</Button>
         </Link>
--- frontend/frontastic/tastics/not-found/index.tsx
+++ frontend/frontastic/tastics/not-found/index.tsx
@@ -2,11 +2,9 @@
 
 import React from 'react';
 import NotFound from 'components/commercetools-ui/organisms/not-found';
-import { TasticProps } from '../types';
-import { ImageProps } from 'components/commercetools-ui/atoms/image';
 
-const NotFoundTastic = ({ data }: TasticProps<{ image: ImageProps }>) => {
-  return <NotFound image={data.image} />;
+const NotFoundTastic = () => {
+  return <NotFound />;
 };
 
 export default NotFoundTastic;
